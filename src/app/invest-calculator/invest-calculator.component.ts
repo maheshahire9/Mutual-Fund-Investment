eecb6c6fc01d2d6a0cec81743b97926e62f710dd
Mahesh Ahire <48119333+maheshahire9@users.noreply.github.com>
2020-07-25 21:55:11 +0530

    changes for return values

--- src/app/invest-calculator/invest-calculator.component.ts
+++ src/app/invest-calculator/invest-calculator.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Validators, FormGroup, FormControl } from "@angular/forms";
 import { HttpClient } from '@angular/common/http';
 
+//ngbDatepicker library installed for datepicker functionality
 import { NgbDateAdapter, NgbDateNativeAdapter } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -10,6 +11,7 @@ import { NgbDateAdapter, NgbDateNativeAdapter } from '@ng-bootstrap/ng-bootstrap
   styleUrls: ['./invest-calculator.component.css'],
   providers: [{ provide: NgbDateAdapter, useClass: NgbDateNativeAdapter }]
 })
+
 export class InvestCalculatorComponent implements OnInit {
 
   schemeName: string;
@@ -19,67 +21,63 @@ export class InvestCalculatorComponent implements OnInit {
   fundData: any [];
   units = 0;
   returns = 0;
-  today
-
+  today: Date;
   submitted:boolean = false;
 
   constructor(private http: HttpClient) { 
     this.today = new Date();
   }
 
-  investmentForm = new FormGroup({
-    amount: new FormControl('',[Validators.required]),
-    startDate: new FormControl('',[Validators.required]),
-    endDate: new FormControl('',[Validators.required]),
-  })  
-
   ngOnInit() {
-    this.http.get('assets/data.json').subscribe((data) => {
-      
-      this.fundData = data as string [];
-      console.log("funddata",this.fundData );
-      this.schemeName = data[0].scheme_name;
-    },(err) => {
-      console.log (err.message);
+    this.http.get('assets/data.json').subscribe((response) => {
+      this.fundData = response as string [];
+      this.schemeName = response[0].scheme_name;
+    },(error) => {
+      console.log(error.message);
     });
   }
 
+  //Required validations for amount, start date and end date
+  investmentForm = new FormGroup({
+    amount: new FormControl('',[Validators.required]),
+    startDate: new FormControl('',[Validators.required]),
+    endDate: new FormControl('',[Validators.required])
+  })
+  
+  //Getting controls for validation function
   get investmentFormControl() {
     return this.investmentForm.controls;
   }
 
+  //Function to calculate invested value
   calculate(){
     this.submitted = true;
     if (this.investmentForm.valid) {
       this.dateFilterForData(this.startDate, this.endDate).then((data) => {
-        console.log("data", data);
-
         this.units = Math.round(this.amount / Number(data[0].nav));
-
-        var resultData = this.processNavValue(data, this.units);
-        console.log("resultData", resultData);
-        console.log("resultData", resultData[resultData.length - 1]);
-        
-        this.returns = Math.round(this.amount - resultData[resultData.length - 1]);
+        var result = this.processNavValue(data, this.units);
+        var lastValue = Number(result[result.length - 1]);
+        this.returns = lastValue - this.amount;
       });
     }
   }
 
-  processNavValue(arr, units) {
-    return arr.map((data) => {
-      const val = data.nav * units;
-      return val;
-    });
-  }
-
+  //Returns data for particular dates ( data between start date & end date)
   dateFilterForData(startDate, endDate){
     return new Promise((resolve) => {
       const data = this.fundData.filter((data) => {        
-        const timestamp = new Date(data.date);
-        return timestamp >= startDate && timestamp <= endDate;
+        const timeData = new Date(data.date);
+        return timeData >= startDate && timeData <= endDate;
       });
       resolve(data)
     });
   }
-
+  
+  //Returns the calculated value from NAV and units
+  processNavValue(arr, units) {
+    return arr.map((data) => {
+      const value = data.nav * units;
+      return value;
+    });
+  }
 }
